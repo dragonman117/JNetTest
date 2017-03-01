15e9ddb59578c16c21c85bfed063a78599f28b95
tophski <toph.kinsey@gmail.com>
2017-03-01 15:36:59 -0700

    Another attempt to merge, now with extra branches!

---
+++ models/Exam.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Chris on 2/21/2017.
+ */
+
+module.exports = function(sequelize, DataTypes) {
+    var Exam = sequelize.define('Exam', {
+            id: {
+                type: DataTypes.INTEGER,
+                unique: true,
+                allowNull: false,
+                primaryKey: true
+            },
+            title: {
+                type: DataTypes.STRING,
+                unique: false,
+                allowNull: false
+            },
+            published: {
+                type: DataTypes.BOOLEAN,
+                unique: false,
+                allowNull: false
+            },
+            open_date: {
+                type: DataTypes.DATE,
+                unique: false,
+                allowNull: true
+            },
+            close_date: {
+                type: DataTypes.DATE,
+                unique: false,
+                allowNull: true
+            },
+            rules_stmt: {
+                type: DataTypes.TEXT,
+                unique: false,
+                allowNull: true
+            },
+            //Time limits will be specified as minutes.
+            time_limit: {
+                type: DataTypes.INTEGER,
+                unique: false,
+                allowNull: true
+            }
+        }
+    )
+    return Exam
+}
---
+++ models/Rubric.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Chris on 2/28/2017.
+ */
+
+module.exports = function(sequelize, DataTypes) {
+    var Rubric = sequelize.define('Rubric', {
+        id: {
+            type: DataTypes.INTEGER,
+            unique: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        data: {
+            type: DataTypes.TEXT,
+            unique: false,
+            allowNull: true,
+            primaryKey: false
+        }
+    })
+    return Rubric;
+}
