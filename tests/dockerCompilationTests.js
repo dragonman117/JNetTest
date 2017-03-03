bddaf584ccdbe8705590da53323cf4108ef92ea1
timothyferrell <tj.ferrell@gmail.com>
2017-03-03 10:57:05 -0700

    Added will not compile test to docker tests

--- tests/dockerCompilationTests.js
+++ tests/dockerCompilationTests.js
@@ -46,6 +46,20 @@ let test2Obj = {
     extraArguments: ""
 };
 
+//Test basic will not compile
+let test3Obj =  {
+    rootpath: "/Users/timothyferrell/WebstormProjects/JNetTest",
+    folder: "tstFolder",
+    fileName: "main.cpp",
+    code: "#include <iostream>\n\nint main(){ \n    cout << \"Hello World\" << endl\n    return 0;\n}",
+    langName: "C++",
+    stdinData: "",
+    vmName: "code-compiler",
+    compilerName: "\'g++ -o /usercode/a.out\' ",
+    outputCommand: "/usercode/a.out",
+    extraArguments: ""
+};
+
 //Tests
 
 test("Compilation: Basic Hello World Compilation Test", t=>{
@@ -73,6 +87,18 @@ test("Compilation: Fork Bomb Test", t=>{
     let tmpObj = new Compilation(test2Obj);
     let expectedData = '\nExecution Timed Out';
     let expectedError = "";
+
+    tmpObj.run().then(result=>{
+        test.is(result.stdout, expectedData);
+        test.is(result.errorLog, expectedError);
+    });
+});
+
+
+test("Compilation: Will Not Compile", t=>{
+    let tmpObj = new Compilation(test2Obj);
+    let expectedData = 'Compilation Failed\n';
+    let expectedError = '/usercode/main.cpp: In function \'int main()\':\n/usercode/main.cpp:4:5: error: \'cout\' was not declared in this scope\n     cout << "Hello World" << endl\n     ^\n/usercode/main.cpp:4:5: note: suggested alternative:\nIn file included from /usercode/main.cpp:1:0:\n/usr/include/c++/5/iostream:61:18: note:   \'std::cout\'\n   extern ostream cout;  /// Linked to standard output\n                  ^\n/usercode/main.cpp:4:30: error: \'endl\' was not declared in this scope\n     cout << "Hello World" << endl\n                              ^\n/usercode/main.cpp:4:30: note: suggested alternative:\nIn file included from /usr/include/c++/5/iostream:39:0,\n                 from /usercode/main.cpp:1:\n/usr/include/c++/5/ostream:590:5: note:   \'std::endl\'\n     endl(basic_ostream<_CharT, _Traits>& __os)\n     ^\n';
 
     tmpObj.run().then(result=>{
         test.is(result.stdout, expectedData);
