12efc318f1fd72dc7aff9f7a29470245c0bdc59b
tophski <toph.kinsey@gmail.com>
2017-04-05 18:13:45 -0600

    Finished for the sprint. No luck fixing section list call

--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -64,31 +64,35 @@ module.exports = function(app, passport) {
      * Get all the test cases from the questions
      */
     app.get('/exam/edit/:id', function (req, res, next) {
-        console.log('Received request for exam edit.');
-        exam = getExamById(req.params["id"]);
-        res.render('exam_edit', exam);
+        getExamById(req.params["id"]).then(exam =>
+        {
+            res.render('exam_edit', exam);
+        });
     });
 };
 
+
 function getExamById(id){
-    var exam_data;
-    let examPromise = db.Exam.findById(id);
-    var questionsPromise = db.Question.findAll({where: {exam_id: 1}})
-    Promise.all([examPromise, questionsPromise]).then(function(results){
-        let exam = results[0];
-        let questions = results[1];
-        let testCasePromises = [];
-        questions.forEach(i => {
-            let promise = db.TestCase.findAll({where:{question_id: i.id}});
-            testCasePromises.push(promise);
-        });
-        Promise.all(testCasePromises).then(testCases => {
-            for(let i = 0; i < testCases.length; i++) {
-                questions[i]['test_cases'] = testCases[i]
-            }
-            exam_data = exam;
-            exam_data['questions'] = questions;
-            return exam;
+    return new Promise((resolve, reject) => {
+        let exam_data;
+        let examPromise = db.Exam.findById(id);
+        var questionsPromise = db.Question.findAll({where: {exam_id: id}})
+        Promise.all([examPromise, questionsPromise]).then(function (results) {
+            let exam = JSON.parse(JSON.stringify(results[0]));
+            let questions = results[1];
+            let testCasePromises = [];
+            questions.forEach(i => {
+                let promise = db.TestCase.findAll({where: {question_id: i.id}});
+                testCasePromises.push(promise);
+            });
+            Promise.all(testCasePromises).then(testCases => {
+                for (let i = 0; i < testCases.length; i++) {
+                    questions[i]['dataValues']['test_cases'] = JSON.parse(JSON.stringify(testCases[i]))
+                }
+                exam_data = exam;
+                exam_data['questions'] = questions;
+                resolve(exam_data);
+            });
         });
     });
 };
